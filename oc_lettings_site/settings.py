fc5e5edb965d8070653da8ced6b28bc22f4db24b
JLenseele <lenseele.julien@gmail.com>
2023-08-02 11:41:07 +0200

    test new config -> push to aws ECR

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -8,10 +8,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('ENV') != 'PRODUCTION':
+    SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+    DEBUG = True
+else:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
